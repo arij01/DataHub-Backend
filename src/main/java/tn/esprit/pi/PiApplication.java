c2d0cb692f39250d5398f65922e5526470e06483
Arij <m'hiri.arij@esprit.tn>
2024-03-03 17:29:23 +0100

    eureka config

--- src/main/java/tn/esprit/pi/PiApplication.java
+++ src/main/java/tn/esprit/pi/PiApplication.java
@@ -3,10 +3,12 @@ package tn.esprit.pi;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 import org.springframework.context.annotation.Bean;
 import tn.esprit.pi.repositories.ColumnRepository;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PiApplication {
 
     public static void main(String[] args) {
